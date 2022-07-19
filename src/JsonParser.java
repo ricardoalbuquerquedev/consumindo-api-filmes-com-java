bb73b1f61e53816e9d0914d4f165d20d537f25d5
Ricardo <ricardoalbuquerquecorretor@gmail.com>
2022-07-19 11:04:43 -0300

    first commit

---
+++ src/JsonParser.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonParser {
+    private static final Pattern REGEX_ITEMS = Pattern.compile("\\[(.+)\\]");
+    private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
+
+    public List<Map<String, String>> parse(String json) {
+        //Pega o que está entre colchetes
+        Matcher matcher = REGEX_ITEMS.matcher(json);
+        if (!matcher.find()) {
+
+            throw new IllegalArgumentException("Não encontrou items.");
+        }
+        //pega os itens e separa
+        String[] items = matcher.group(1).split("\\},\\{");
+
+        List<Map<String, String>> dados = new ArrayList<>();
+        //Para cada item, separa os atributos. Adiciona tudo em uma lista de maps
+        for (String item : items) {
+
+            Map<String, String> atributosItem = new HashMap<>();
+
+            Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+            while (matcherAtributosJson.find()) {
+                String atributo = matcherAtributosJson.group(1);
+                String valor = matcherAtributosJson.group(2);
+                atributosItem.put(atributo, valor);
+            }
+
+            dados.add(atributosItem);
+        }
+
+        return dados;
+    }
+}
